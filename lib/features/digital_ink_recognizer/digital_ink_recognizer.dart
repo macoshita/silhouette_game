595c82082e14542ddc627ec9cf37c93edbdbf2dc
macoshita <macoshita@users.noreply.github.com>
2024-01-07 10:22:39 +0900

    一部のカタカナが入力できなかったのを修正

--- lib/features/digital_ink_recognizer/digital_ink_recognizer.dart
+++ lib/features/digital_ink_recognizer/digital_ink_recognizer.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:google_mlkit_digital_ink_recognition/google_mlkit_digital_ink_recognition.dart'
     as r;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:silhouette_game/features/game_mode/game_mode.dart';
 import 'package:silhouette_game/features/handwritten_cell/time_series_offset.dart'
     show Character;
 
@@ -15,7 +17,7 @@ Future<r.DigitalInkRecognizer> digitalInkRecognizer(
   if (!downloaded) {
     await modelManager.downloadModel(languageModel);
   }
-  return r.DigitalInkRecognizer(languageCode: 'JA');
+  return r.DigitalInkRecognizer(languageCode: languageModel);
 }
 
 typedef RecognizeFunction = Future<bool> Function(Character, String);
@@ -45,9 +47,32 @@ RecognizeFunction recognize(RecognizeRef ref) {
     final result = await recognizer.recognize(ink);
     print(result.map((e) => e.text).toList());
 
-    // 最初の 2 候補までを正解とする
-    return result.take(2).any((e) =>
-        e.text == expected ||
-        (expected == 'ー' && ['一', '-', 'ー', '_'].contains(e.text)));
+    final gameMode = ref.read(gameModeProvider);
+    final filtered = result.where(
+      (e) {
+        // 2 文字以上は無視
+        if (e.text.characters.length != 1) return false;
+
+        // ハイフンは特定のものだけ残す
+        if (e.text == 'ー') return true;
+
+        // ゲームモードに応じてひらがな・カタカナのみ残す
+        return switch (gameMode) {
+          GameMode.hiragana => _isHiragana(e.text),
+          GameMode.katakana => _isKatakana(e.text),
+        };
+      },
+    );
+    print(filtered.map((e) => e.text).toList());
+
+    return filtered.take(2).any((e) => e.text == expected);
   };
+}
+
+bool _isHiragana(String text) {
+  return text.codeUnitAt(0) >= 0x3040 && text.codeUnitAt(0) <= 0x309F;
+}
+
+bool _isKatakana(String text) {
+  return text.codeUnitAt(0) >= 0x30A1 && text.codeUnitAt(0) <= 0x30FC;
 }
